d74f42f6531b714898698635627581d272bbeb03
lele <1789335298@qq.com>
2025-11-11 16:36:05 +0800

    version 0.10.1

--- prompt-fission.user.js
+++ prompt-fission.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Prompt Fission
 // @namespace    http://tampermonkey.net/
-// @version      0.10.0
+// @version      0.10.1
 // @description  Enhances chat interfaces with prompt fission capabilities.
 // @author       lele
 // @match        https://chat.deepseek.com/*
